4ac3e00d0414214a549138b19b52c719d30cf9d8
whatishadoop <jinlu.hansome@163.com>
2019-07-31 23:07:04 +0800

    首页修改

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -35,7 +35,8 @@
       <!-- 态势案件 -->
       <div class="situationcase-wrapper">
         <div class="situationcase-title">
-          <span class="title">态势案件</span><span class="subtitle">群体洞察</span>
+          <span class="title">态势案件</span>
+          <span class="subtitle">群体案件</span>
         </div>
         <ul class="case">
           <li v-for="(value,index) in [1,2,3]" :key="index" class="case-item">
@@ -43,11 +44,41 @@
             <div class="content">
               <div class="name">湖南省公安厅</div>
               <div class="desc">
-                <span class="count">案件号 9A757DGJK34V5JDG</span><span class="date">2018.04.23</span>
+                <span class="count">案件号 9A757DGJK34V5JDG</span>
+                <span class="date">2018.04.23</span>
               </div>
             </div>
           </li>
         </ul>
+        <div class="morecase">
+          查看更多
+        </div>
+      </div>
+
+      <!-- 态势感知 -->
+      <div class="situationcase-wrapper">
+        <div class="situationcase-title">
+          <span class="title">态势感知</span>
+          <span class="subtitle">群体洞察</span>
+        </div>
+        <ul class="case">
+          <li v-for="(value,index) in [1,2,3]" :key="index" class="case-item">
+            <div class="icon">X</div>
+            <div class="content">
+              <div class="name">湖南省公安厅</div>
+              <div class="text">MSISDN：8613501978189</div>
+              <div class="text">IMSI：460006234567890</div>
+              <div class="text">IMEI：109698888877777</div>
+              <div class="desc">
+                <span class="count">案件号 9A757DGJK34V5JDG</span>
+                <span class="date">2018.04.23</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <div class="morecase">
+          查看更多
+        </div>
       </div>
     </div>
   </div>
@@ -55,143 +86,183 @@
 <script>
 </script>
 <style type="text/scss" rel="stylesheet/scss" lang="scss" scoped>
-@import "~@/styles/mixin.scss";
+  @import "~@/styles/mixin.scss";
 
-.header {
-	position: relative;
+  .header {
+    position: relative;
     overflow: hidden;
     top: 0;
     left: 0;
     width: 100%;
-    .content-wrapper {
-    	height: 44px;
-    	padding: 4px 20px 4px 20px
-    }
-}
-.content {
-	position: relative;
-	width: 100%;
-	.classify-wrapper {
-		margin-top: 25px;
-		background: #fff;
-		.classify {
-			overflow: hidden;
-			padding: 0;
-			margin: 0;
-            .classify-item {
-            	float: left;
-				width: 25%;
-				text-align: center;
-				list-style: none;
-				.icon {
-					display: inline-block;
-					width: 45px;
-					height: 45px;
-					line-height: 45px;
-					text-align: center;
-					border-radius: 50%;
-					background: #4a95dc;
-					font-size: 18px;
-					color: #fff;
-					margin: 0 0 10px 0;
-				}
-				.list {
-					color: #333333;
-					margin: 0;
-                    font-size: 14px;
-				}
-            }
-		}
-	}
-	.bulletin-wrapper {
-		position: relative;
-		height: 20px;
-		line-height: 20px;
-		margin-top: 32px;
-		padding: 0 20px 0 20px;
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-		background: #fff;
 
-		.bulletin-title {
-	        display: inline-block;
-	        vertical-align: top;
-	        margin-top: 8px;
-	        width: 22px;
-	        height: 12px;
-	        /* @include bg-image('bulletin');*/
-	        background-size: 22px 12px;
-	        background-repeat: no-repeat;
-        }
-        .bulletin-text {
-	        vertical-align: top;
-	        margin: 0 4px;
-	        height: 20px;
-	        line-height: 20px;
-	        font-size: 14px;
-        }
-	}
-	.situationcase-wrapper {
-        margin-top: 32px;
-	        .situationcase-title {
-				padding-left: 16px;
-			        .title {
-			        	font-size: 20px;
-			        	line-height: 28px;
-			        	color: #333333;
-			        }
-			        .subtitle {
-			        	margin-left: 8px;
-			        	font-size: 15px;
-			        	line-height: 21px;
-			        }
-	        }
-	        .case {
-	        	margin: 0;
-	        	padding-left: 20px;
-	        	padding-right: 16px;
-	        	.case-item {
-	        		display: flex;
-			        margin: 14px 0px 14px 0px;
-			        list-style: none;
-			        @include border-1px(rgba(7, 17, 27, 0.1));
-			        .icon {
-	                    display: inline-block;
-			            vertical-align: top;
-			            width: 10px;
-			            height: 10px;
-			            margin-right: 9px;
-			            /* @include bg-image('decrease_3'); */ /*背景图片设置必须是块元素*/
-			            background-size: 10px 10px;
-			            background-repeat: no-repeat;
-			        }
-                    .content {
-			          flex: 1;
-			            .name {
-							margin: 2px 0 8px 0;
-							height: 25px;
-							line-height: 25px;
-							font-size: 18px;
-							color: #161619;
-						}
-						.desc {
-							margin-bottom: 8px;
-							.count {
-							  margin-right: 59px;
-							  line-height: 14px;
-							  font-size: 14px;
-							  color: #161619;
-							}
-							.date {
-							  line-height: 18px;
-							  font-size: 13px;
-							  color: #000000;
-							}
-						}
-					}
-				}
-	        }
+    .content-wrapper {
+      height: 44px;
+      padding: 4px 20px 4px 20px
     }
-}
+  }
+
+  .content {
+    position: relative;
+    width: 100%;
+
+    .classify-wrapper {
+      margin-top: 25px;
+      background: #fff;
+
+      .classify {
+        overflow: hidden;
+        padding: 0;
+        margin: 0;
+
+        .classify-item {
+          float: left;
+          width: 25%;
+          text-align: center;
+          list-style: none;
+
+          .icon {
+            display: inline-block;
+            width: 45px;
+            height: 45px;
+            line-height: 45px;
+            text-align: center;
+            border-radius: 50%;
+            background: #4a95dc;
+            font-size: 18px;
+            color: #fff;
+            margin: 0 0 10px 0;
+          }
+
+          .list {
+            color: #333333;
+            margin: 0;
+            font-size: 14px;
+          }
+        }
+      }
+    }
+
+    .bulletin-wrapper {
+      position: relative;
+      height: 20px;
+      line-height: 20px;
+      margin-top: 32px;
+      padding: 0 20px 0 20px;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      background: #fff;
+
+      .bulletin-title {
+        display: inline-block;
+        vertical-align: top;
+        margin-top: 8px;
+        width: 22px;
+        height: 12px;
+        /* @include bg-image('bulletin');*/
+        background-size: 22px 12px;
+        background-repeat: no-repeat;
+      }
+
+      .bulletin-text {
+        vertical-align: top;
+        margin: 0 4px;
+        height: 20px;
+        line-height: 20px;
+        font-size: 14px;
+      }
+    }
+
+    .situationcase-wrapper {
+      margin-top: 32px;
+
+      .situationcase-title {
+        padding-left: 16px;
+
+        .title {
+          font-size: 20px;
+          line-height: 28px;
+          color: #333333;
+        }
+
+        .subtitle {
+          margin-left: 8px;
+          font-size: 15px;
+          line-height: 21px;
+        }
+      }
+
+      .case {
+        margin: 0;
+        padding-left: 20px;
+        padding-right: 16px;
+
+        .case-item {
+          display: flex;
+          margin: 14px 0px 14px 0px;
+          list-style: none;
+          @include border-1px(rgba(7, 17, 27, 0.1));
+
+          .icon {
+            display: inline-block;
+            vertical-align: top;
+            width: 10px;
+            height: 10px;
+            margin-right: 9px;
+            /* @include bg-image('decrease_3'); */
+            /*背景图片设置必须是块元素*/
+            background-size: 10px 10px;
+            background-repeat: no-repeat;
+          }
+
+          .content {
+            flex: 1;
+
+            .name {
+              margin: 2px 0 8px 0;
+              height: 25px;
+              line-height: 25px;
+              font-size: 18px;
+              color: #161619;
+            }
+
+            .text {
+              height: 20px;
+              line-height: 20px;
+              font-size: 14px;
+              color: #161619;
+              &:last-child {
+                margin-bottom: 13px;
+              }
+            }
+
+            .desc {
+              margin-bottom: 8px;
+              .count {
+                margin-right: 59px;
+                line-height: 14px;
+                font-size: 14px;
+                color: #161619;
+              }
+
+              .date {
+                line-height: 18px;
+                font-size: 13px;
+                color: #000000;
+              }
+            }
+          }
+        }
+      }
+      .morecase {
+        margin: 18px 13px 26px 13px;
+        height: 47px;
+        line-height: 47px;
+        text-align: center;
+        background: #F2F3F7;
+        font-size: 18px;
+        color: #999999;
+      }
+    }
+  }
 </style>
